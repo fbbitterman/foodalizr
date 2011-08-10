62dc5a2f984e353fc0f2a3804c10390e13c7311d
David Fuhr <dcf@knid.net>
2011-08-11 00:11:28 +0200

    [~TASK] updated autoload, added example index, ignore build dir

--- web/index.php
+++ web/index.php
@@ -1,14 +1,30 @@
 <?php
 
+$config = include '../src/config.php';
+
 require_once implode(DIRECTORY_SEPARATOR, array(
     dirname(__FILE__), '..', 'src', 'autoload.php'
 ));
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+$db = new \mysqli($config['db']['host'], $config['db']['user'],
+    $config['db']['password'], $config['db']['name']);
+$db->query('SET NAMES utf8;');
+$mapperFactory = new \Foodalizr\Model\MapperFactory($db);
+
+$request = new \Knid\Http\Request(array(
+    'cookie' => $_COOKIE,
+    'env' => $_ENV,
+    'files' => $_FILES,
+    'get' => $_GET,
+    'post' => $_POST,
+    'server' => $_SERVER,
+));
+$response = new \Knid\Http\Response();
+$response->addHeader(new \Knid\Http\Header('Content-Type', 'text/html; charset=utf-8'));
+$response->setContent('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
     <title>Foodalizr</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 
@@ -17,12 +33,6 @@ require_once implode(DIRECTORY_SEPARATOR, array(
     <button class="add person-add">Person</button>
 </p>
 
-<div id="person-add-dialog" title="Person hinzufügen">
-<form>
-    <label for="person-name">Name</label>
-    <input type="text" name="name" id="person-name" />
-</form>
-</div>
-
 </body>
-</html>
+</html>');
+$response->send();
